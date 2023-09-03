6b700d2c5afe7c30a0b3aa5a0db9873029da48ab
EnverJ <Enlem1987@gmail.com>
2023-09-02 18:18:21 -0600

    this trasit

--- src/main/java/io/ezbrains/Employee.java
+++ src/main/java/io/ezbrains/Employee.java
@@ -1,16 +1,53 @@
 package io.ezbrains;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.sun.jdi.PrimitiveValue;
+
+import javax.persistence.*;
+import javax.xml.crypto.Data;
+import java.util.Date;
 
 @Entity
-@Table(name = "EMPLOYEE_DATA")
+@Table(name = "EMPLOYEE_DATA") // (table name is optional, by default it is class name)
 public class Employee {
 
-   @Id
-    private  int id;
-    private  String name;
+    @Id   // primary key
+    private int id;
+    @Column(name = "employee_name", unique = false)  // unique mean if the value is duplicated
+    private String name;
+    private int age;
+    @Column(unique = true, length = 10, nullable = false, updatable = false)
+    private String ssn;
+    @Temporal(TemporalType.TIME)
+    private Date dob;
+    @Enumerated(EnumType.STRING)
+    private EmployeeType type;
+    @Transient// is not going to be saved
+    private String debugString;
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    @Column(insertable = false)
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
 
     public int getId() {
         return id;
@@ -28,6 +65,12 @@ public class Employee {
         this.name = name;
     }
 
+    public EmployeeType getType() {
+        return type;
+    }
 
 
+    public void setType(EmployeeType type) {
+        this.type = type;
+    }
 }
--- src/main/java/io/ezbrains/JpaStarterMain.java
+++ src/main/java/io/ezbrains/JpaStarterMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.xml.crypto.Data;
+import java.util.Date;
 
 public class JpaStarterMain {
 
@@ -12,10 +14,26 @@ public class JpaStarterMain {
         Employee employee = new Employee();
         employee.setId(1);
         employee.setName("Foo Bar");
+        employee.setSsn("233");
+        employee.setDob(new Date());
+        employee.setType(EmployeeType.FULL_TIME);
 
-        // create an entity manager factory
-       EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
-       EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Employee employee1 = new Employee();
+        employee1.setId(2);
+        employee1.setName("Foo Baz");
+        employee1.setSsn("2303");
+        employee1.setType(EmployeeType.CONTRACTOR);
+
+
+        Employee employee3 = new Employee();
+        employee3.setId(2);
+        employee3.setName("Foo Bazar");
+        employee3.setSsn("2303");
+
+
+        // create an entity manager factory. is a service to manage @Entity (data holder)
+       EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp"); // persistence is a class and it will connect to persistence.xml
+       EntityManager entityManager = entityManagerFactory.createEntityManager(); //save Entity
        EntityTransaction transaction = entityManager.getTransaction();
        transaction.begin();
        entityManager.persist(employee);
