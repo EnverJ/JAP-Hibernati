c060a08cea6e3b9470d151412af7adf7bd6267a3
EnverJ <Enlem1987@gmail.com>
2023-09-17 21:22:53 -0600

    jpa starter update class added for manyToMnay relationship

--- src/main/java/io/ezbrains/JpaStarterUpdate.java
+++ src/main/java/io/ezbrains/JpaStarterUpdate.java
@@ -6,9 +6,34 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaStarterUpdate {
-    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp"); // persistence is a class, and it will connect to persistence.xml
-    EntityManager entityManager = entityManagerFactory.createEntityManager(); //save Entity
-    EntityTransaction transaction = entityManager.getTransaction();
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp"); // persistence is a class, and it will connect to persistence.xml
+        EntityManager entityManager = entityManagerFactory.createEntityManager(); //save Entity
+         // fetch data
+        Employee employee = entityManager.find(Employee.class,2);
+        EmailGroup emailGroup =  entityManager.find(EmailGroup.class,8);
+
+        employee.addEmailSubscription(emailGroup);
+        emailGroup.addMember(employee);
+
+
+
+
+
+        EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
+        entityManager.persist(emailGroup);
+        entityManager.persist(employee);
+
+
+
+
+
+
+        transaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
 
 }
