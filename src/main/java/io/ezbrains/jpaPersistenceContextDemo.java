0058790c7050034c20840fc4707a5c4ec0df109c
EnverJ <Enlem1987@gmail.com>
2023-09-22 22:26:03 -0600

    persistence template

--- src/main/java/io/ezbrains/jpaPersistenceContextDemo.java
+++ src/main/java/io/ezbrains/jpaPersistenceContextDemo.java
@@ -12,85 +12,25 @@ public class jpaPersistenceContextDemo {
     public static void main(String[] args) {
 
         Employee employee = new Employee();
-        employee.setName("Foo Bar");
-        employee.setSsn("233");
+        employee.setName("new employee");
+        employee.setSsn("233ss");
         employee.setDob(new Date());
-        employee.setAge(20);
+        employee.setAge(24);
         employee.setType(EmployeeType.FULL_TIME);
-
-        Employee employee1 = new Employee();
-        employee1.setName("Foo Baz");
-        employee1.setSsn("2303");
-        employee1.setType(EmployeeType.CONTRACTOR);
-        employee1.setAge(30);
-
-        // Access Card
-        AccessCard card1 = new AccessCard();
-        card1.setIssueDate(new Date());
-        card1.setActive(true);
-        card1.setFirmwareVersion("1.0.0");
-        employee.setCard(card1);
-        employee1.setCard(card1);
-        card1.setOwner(employee);
-
-
-        AccessCard card2 = new AccessCard();
-        card2.setIssueDate(new Date());
-        card2.setActive(false);
-        card2.setFirmwareVersion("1.2.0");
-        card2.setId(20);
-        card1.setOwner(employee1);
-        employee1.setCard(card2);
-
-        PayStub payStub1 = new PayStub();
-        payStub1.setPayPeriodStart(new Date());
-        payStub1.setPayPeriodEnd(new Date());
-        payStub1.setEmployee(employee);
-        payStub1.setSalary(1000);
-
-        PayStub payStub2 = new PayStub();
-        payStub2.setPayPeriodStart(new Date());
-        payStub2.setPayPeriodEnd(new Date());
-        payStub2.setEmployee(employee1);
-        payStub2.setSalary(2000);
-
-        EmailGroup group = new EmailGroup();
-        group.setName("Company Watercooler Discussion");
-        group.addMember(employee);
-        group.addMember(employee1);
-        employee.addEmailSubscription(group);
-        employee1.addEmailSubscription(group);
-
-        EmailGroup group1 = new EmailGroup();
-        group1.setName("Engineering");
-        employee.addEmailSubscription(group1);
-        group1.addMember(employee);
-
-
-
-
-        employee.setPayStub(List.of(payStub1, payStub2));
-
-
+        System.out.println("*****************Create employee instance********");
 
         // create an entity manager factory. is a service to manage @Entity (data holder)
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp"); // persistence is a class, and it will connect to persistence.xml
         EntityManager entityManager = entityManagerFactory.createEntityManager(); //save Entity
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
+        System.out.println("************Starting transition**********");
+
+
         entityManager.persist(employee);
-        entityManager.persist(employee1);
-
-        entityManager.persist(card1);
-        entityManager.persist(card2);
-
-        entityManager.persist(payStub1);
-        entityManager.persist(payStub2);
-
-        entityManager.persist(group);
-        entityManager.persist(group1);
-
+        System.out.println("************After Persist method called**********");
         transaction.commit();
+        System.out.println("************After Persist method transition**********");
         entityManager.close();
         entityManagerFactory.close();
 
